814483a07da79068f1964bb02fba57ac1857a713
bencarneiro <bencarneiro@gmail.com>
2023-09-18 07:33:32 -0700

    add comment with the variables in teh accident table

--- data/fatalities/models.py
+++ data/fatalities/models.py
@@ -20,6 +20,88 @@ class County(models.Model):
         db_table = "county"
         managed = True
 
+class Accident(models.Model):
+    number_of_forms_submitted_for_persons_not_in_motor_vehicles
+    number_of_forms_submitted_for_persons_not_in_motor_vehicles_in_transit
+    
+
+
+# The ACCIDENT Data File 33
+# C3 Number of Forms Submitted for Persons
+# Not in Motor Vehicles PEDS 34
+# C3A Number of Persons Not in Motor Vehicles
+# In-Transport (MVIT) PERNOTMVIT 34
+# C4 Number of Vehicle Forms Submitted- ALL VE_TOTAL 35
+# C4A Number of Motor Vehicles In-Transport (MVIT) VE_FORMS 36
+# C4B Number of Parked/Working Vehicles PVH_INVL 37
+# C5 Number of Forms Submitted for Persons
+# in Motor Vehicles PERSONS 38
+# C5A Number of Persons in Motor Vehicles
+# In-Transport (MVIT) PERMVIT 39
+# C6 County COUNTY 40
+# C7 City CITY 41
+# C8A Month of Crash MONTH 42
+# C8B Day of Crash DAY 42
+# C8C Day of Week DAY_WEEK 43
+# C8D Year of Crash YEAR 43
+# C9A Hour of Crash HOUR 44
+# C9B Minute of Crash MINUTE 44
+# C10 Trafficway Identifier TWAY_ID 45
+# C10 Trafficway Identifier TWAY_ID2 45
+# C11 Route Signing ROUTE 46
+# C12A Land Use RUR_URB 47
+# C12B Functional System FUNC_SYS 47
+# 13
+# C13 Ownership RD_OWNER 48
+# C14 National Highway System NHS 49
+# C15 Special Jurisdiction SP_JUR 50
+# C16 Milepoint MILEPT 51
+# C17A Latitude LATITUDE 52
+# C17B Longitude LONGITUD 53
+# C19 First Harmful Event HARM_EV 54
+# C20 Manner of Collision of the First Harmful Event MAN_COLL 58
+# C21A Relation to Junction- Within Interchange Area RELJCT1 60
+# C21B Relation to Junction- Specific Location RELJCT2 61
+# C22 Type of Intersection TYP_INT 63
+# C23 Relation to Trafficway REL_ROAD 64
+# C24 Work Zone WRK_ZONE 65
+# C25 Light Condition LGT_COND 66
+# C26 Atmospheric Conditions WEATHER 67
+# C27 School Bus Related SCH_BUS 69
+# C28 Rail Grade Crossing Identifier RAIL 70
+# C29A Hour of Notification NOT_HOUR 71
+# C29B Minute of Notification NOT_MIN 71
+# C30A Hour of Arrival at Scene ARR_HOUR 72
+# C30B Minute of Arrival at Scene ARR_MIN 72
+# C31A Hour of EMS Arrival at Hospital HOSP_HR 73
+# C31B Minute of EMS Arrival at Hospital HOSP_MN 73
+# C101 Fatalities FATALS 74
+# Atmospheric Conditions (discontinued) WEATHER1 75
+# Atmospheric Conditions (discontinued) WEATHER2 75
+# Federal Highway (discontinued) FED_AID 76
+# Hit-and-Run (discontinued) HIT_RUN 77
+# Land Use (discontinued) LAND_USE 78
+# Number of Drinking Drivers (discontinued) DRUNK_DR 79
+# Related Factors- Crash Level (discontinued) CF1 80
+# Related Factors- Crash Level (discontinued) CF2 80
+# Related Factors- Crash Level (discontinued) CF3 80
+# Roadway Alignment (discontinued) ALIGNMNT 82
+# Roadway Function Class (discontinued) ROAD_FNC 83
+# Roadway Profile (discontinued) PROFILE 84
+# Roadway Surface Condition (discontinued) SUR_COND 84
+# Roadway Surface Type (discontinued) PAVE_TYP 85
+# Speed Limit (discontinued) SP_LIMIT 85
+# 14
+# Total Lanes in Roadway (discontinued) NO_LANES 86
+# Traffic Control Device (discontinued) TRA_CONT 87
+# Traffic Control Device Functioning
+# (discontinued) T_CONT_F 89
+# Trafficway Description (discontinued) TRAF_FLO 89
+# Vehicles In-Transport (discontinued) VEHICLES 90
+
+
+
+
 
 
 # class Accident(models.Model):
