fbbeef081afaf14e64109b4c42ecbd7183941c1e
bencarneiro <bencarneiro@gmail.com>
2024-05-23 21:55:29 -0500

    fix vehicle related factors etl script

--- data/fatalities/management/commands/2022_pvehiclesf.py
+++ data/fatalities/management/commands/2022_pvehiclesf.py
@@ -5,14 +5,14 @@ from fatalities.models import ParkedVehicleRelatedFactor, Accident, ParkedVehicl
 class Command(BaseCommand):
     def handle(self, *args, **kwasrgs):
         ParkedVehicleRelatedFactor.objects.filter(parked_vehicle__accident__year=2022).delete()
-        csv = pd.read_csv("/home/tonydeals/app/ntsb/data/csvs/2022/vehiclesf.csv", encoding='latin-1')
+        csv = pd.read_csv("/home/tonydeals/app/ntsb/data/csvs/2022/pvehiclesf.csv", encoding='latin-1')
         for x in csv.index:
-            accident = Accident.objects.get(year=2022, st_case=csv['ST_CASE'][x])
+            print(csv['ST_CASE'][x])
+            print(csv['VEH_NO'][x])
             parked_vehicle = ParkedVehicle.objects.get(accident__year=2022, accident__st_case=csv['ST_CASE'][x], vehicle_number=csv['VEH_NO'][x])
             data_to_save = {
-                "accident": accident,
                 "parked_vehicle": parked_vehicle,
-                "vehicle_related_factor": csv['PVEHICLESF'][x]
+                "parked_vehicle_related_factor": csv['PVEHICLESF'][x]
             }
             print(data_to_save)
             ParkedVehicleRelatedFactor.objects.create(**data_to_save)
--- data/fatalities/management/commands/2022_vehiclesf.py
+++ data/fatalities/management/commands/2022_vehiclesf.py
@@ -4,13 +4,11 @@ from fatalities.models import VehicleRelatedFactor, Accident, Vehicle
 
 class Command(BaseCommand):
     def handle(self, *args, **kwasrgs):
-        VehicleRelatedFactor.objects.filter(accident__year=2022).delete()
+        VehicleRelatedFactor.objects.filter(vehicle__accident__year=2022).delete()
         csv = pd.read_csv("/home/tonydeals/app/ntsb/data/csvs/2022/vehiclesf.csv", encoding='latin-1')
         for x in csv.index:
-            accident = Accident.objects.get(st_case=csv['ST_CASE'][x])
-            vehicle = Vehicle.objects.get(st_case=csv['ST_CASE'][x], vehicle_number=csv['VEH_NO'][x])
+            vehicle = Vehicle.objects.get(accident__year=2022, accident__st_case=csv['ST_CASE'][x], vehicle_number=csv['VEH_NO'][x])
             data_to_save = {
-                "accident": accident,
                 "vehicle": vehicle,
                 "vehicle_related_factor": csv['VEHICLESF'][x]
             }
