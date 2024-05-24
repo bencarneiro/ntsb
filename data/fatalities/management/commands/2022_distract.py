fba44367fc84c9f99eafa8f6a3013cd1e525647b
bencarneiro <bencarneiro@gmail.com>
2024-05-24 15:07:09 -0500

    add 2022 etl script for distracted drivers

--- data/fatalities/management/commands/2022_distract.py
+++ data/fatalities/management/commands/2022_distract.py
@@ -1,14 +1,14 @@
 from django.core.management.base import BaseCommand
 import pandas as pd
-from fatalities.models import Damage, Vehicle
+from fatalities.models import DriverDistracted, Vehicle
 
 class Command(BaseCommand):
     def handle(self, *args, **kwasrgs):
-        Damage.objects.filter(vehicle__accident__year=2022).delete()
-        csv = pd.read_csv("/home/tonydeals/app/ntsb/data/csvs/2022/damage.csv", encoding='latin-1')
+        DriverDistracted.objects.filter(vehicle__accident__year=2022).delete()
+        csv = pd.read_csv("/home/tonydeals/app/ntsb/data/csvs/2022/distract.csv", encoding='latin-1')
         for x in csv.index:
             vehicle = Vehicle.objects.get(accident__st_case=csv['ST_CASE'][x], vehicle_number=csv['VEH_NO'][x])
             data_to_save = {"vehicle": vehicle}
-            data_to_save['area_of_impact'] = csv['DAMAGE'][x]
+            data_to_save['distracted_by'] = csv['DRDISTRACT'][x]
             print(data_to_save)
-            Damage.objects.create(**data_to_save)
+            DriverDistracted.objects.create(**data_to_save)
