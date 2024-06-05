a4cb3c8f5a50460e102e63d0a48573f838e27d3c
bencarneiro <bencarneiro@gmail.com>
2024-06-05 15:31:11 -0500

    turn off debug mode

--- data/data/settings.py
+++ data/data/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$@0+!h+srs+u_q&2k98050j!8=ym=w(%^+b+6(&c1z=ui(6)94"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (bool(int(os.environ.get('DEBUG',1))))
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "roadway.report", "www.roadway.report"]
 
