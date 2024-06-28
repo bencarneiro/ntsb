cb8e5d65f9fb84505c33a4a57dc95ea6934df46b
bencarneiro <bencarneiro@gmail.com>
2024-06-28 16:15:00 -0500

    import 2019 parked vehicle related factors

---
+++ data/fatalities/management/commands/2019_pvehiclesf.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand
+from data.settings import CSV_PATH
+import pandas as pd
+from fatalities.data_processing import get_data_source, parked_vehicle_related_factor_converter
+from fatalities.models import ParkedVehicleRelatedFactor, Accident, ParkedVehicle
+
+# multiple sources before 2019
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwasrgs):
+        ParkedVehicleRelatedFactor.objects.filter(parked_vehicle__accident__year=2019).delete()
+        csv = pd.read_csv(f"{CSV_PATH}2019/FARS2019NationalCSV/Parkwork.CSV", encoding='latin-1')
+        for x in csv.index:
+            parked_vehicle = ParkedVehicle.objects.get(accident__year=2019, accident__st_case=csv['ST_CASE'][x], vehicle_number=csv['VEH_NO'][x])
+            st_case = str(csv['ST_CASE'][x])
+            if len(st_case) == 5:
+                st_case = "0" + st_case
+            veh_no = str(csv['VEH_NO'][x])
+            while len(veh_no) < 3:
+                veh_no = "0" + veh_no
+            for factor in ["PVEH_SC1", "PVEH_SC2"]:
+                number_saved = len(ParkedVehicleRelatedFactor.objects.filter(parked_vehicle=parked_vehicle))
+                new_factor_id = str(number_saved + 1)
+                while len(new_factor_id) < 3:
+                    new_factor_id = "0" + new_factor_id
+                primary_key = f"2019{st_case}{veh_no}{new_factor_id}"
+                # data_source = get_data_source("parked_vehicle_related_factor.vehicle_related_factor", 2019)
+                # csv_field_name = data_source.split(".")[1]
+                factor_code = parked_vehicle_related_factor_converter(csv[factor][x], 2019)
+                if factor_code:
+                    data_to_save = {
+                        "id": primary_key,
+                        "parked_vehicle": parked_vehicle,
+                        "parked_vehicle_related_factor": factor_code
+                    }
+                    print(data_to_save)
+                    ParkedVehicleRelatedFactor.objects.create(**data_to_save)
