638e592862847686184b95eb49e701eac1c00c23
bencarneiro <bencarneiro@gmail.com>
2024-07-03 23:12:24 -0500

    import 2014 driver impairments

---
+++ data/fatalities/management/commands/2014_drimpair.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+from data.settings import CSV_PATH
+import pandas as pd
+from fatalities.data_processing import get_data_source, driver_impaired_converter
+from fatalities.models import DriverImpaired, Vehicle
+
+
+# found elsewhere before 2010
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwasrgs):
+        DriverImpaired.objects.filter(vehicle__accident__year=2014).delete()
+        csv = pd.read_csv(f"{CSV_PATH}2014/FARS2014NationalCSV/DRIMPAIR.CSV", encoding='latin-1')
+        for x in csv.index:
+            vehicle = Vehicle.objects.get(accident__year=2014, accident__st_case=csv['ST_CASE'][x], vehicle_number=csv['VEH_NO'][x])
+
+            st_case = str(csv['ST_CASE'][x])
+            if len(st_case) == 5:
+                st_case = "0" + st_case
+            veh_no = str(csv['VEH_NO'][x])
+            while len(veh_no) < 3:
+                veh_no = "0" + veh_no
+            number_saved = len(DriverImpaired.objects.filter(vehicle=vehicle))
+            new_impairment_id = str(number_saved + 1)
+            while len(new_impairment_id) < 3:
+                new_impairment_id = "0" + new_impairment_id
+            primary_key = f"2014{st_case}{veh_no}{new_impairment_id}"
+
+            data_to_save = {"vehicle": vehicle, "id": primary_key}
+            data_source = get_data_source("driver_impaired.driver_impaired", 2014)
+            csv_field_name = data_source.split(".")[1]
+            data_to_save['driver_impaired'] = driver_impaired_converter(csv[csv_field_name][x] ,2014)
+            print(data_to_save)
+            DriverImpaired.objects.create(**data_to_save)
