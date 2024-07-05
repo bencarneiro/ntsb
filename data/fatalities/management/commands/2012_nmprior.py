12f383be69b32084b804b462cd2287223a1423b0
bencarneiro <bencarneiro@gmail.com>
2024-07-05 10:52:30 -0500

    import remaining three nonmotorist tables for 2012

---
+++ data/fatalities/management/commands/2012_nmprior.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+from data.settings import CSV_PATH
+import pandas as pd
+from fatalities.data_processing import get_data_source
+from fatalities.models import NonmotoristPriorAction, Person
+from django.db.models import Q
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwasrgs):
+        NonmotoristPriorAction.objects.filter(person__accident__year=2012).delete()
+        csv = pd.read_csv(f"{CSV_PATH}2012/FARS2012NationalCSV/NMPRIOR.CSV", encoding='latin-1')
+        for x in csv.index:
+            person = Person.objects.get(accident__year=2012, person_number=csv['PER_NO'][x], accident__st_case=csv['ST_CASE'][x], vehicle__vehicle_number__isnull=True, parked_vehicle__vehicle_number__isnull=True)
+            
+            st_case = str(csv['ST_CASE'][x])
+            if len(st_case) == 5:
+                st_case = "0" + st_case
+            veh_no = str(csv['VEH_NO'][x])
+            while len(veh_no) < 3:
+                veh_no = "0" + veh_no
+            per_no = str(csv['PER_NO'][x])
+            while len(per_no) < 3:
+                per_no = "0" + per_no
+            number_saved = len(NonmotoristPriorAction.objects.filter(person=person))
+            new_factor_id = str(number_saved + 1)
+            while len(new_factor_id) < 3:
+                new_factor_id = "0" + new_factor_id
+            primary_key = f"2012{st_case}{veh_no}{per_no}{new_factor_id}"
+            data_to_save = {"id": primary_key, "person": person}
+            data_source = get_data_source("nonmotorist_prior_action.nonmotorist_prior_action", 2012)
+            csv_field_name = data_source.split(".")[1]
+            data_to_save['nonmotorist_prior_action'] = csv[csv_field_name][x]
+            print(data_to_save)
+            NonmotoristPriorAction.objects.create(**data_to_save)
