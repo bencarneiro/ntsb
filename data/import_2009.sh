f0ef7a21044d381255d9d696a880a4e91b1141cd
bencarneiro <bencarneiro@gmail.com>
2024-07-08 09:52:29 -0500

    fix bash script to run the right scripts (Holy shit I accidentally just deleted 2010 in production

--- data/import_2009.sh
+++ data/import_2009.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 export CSV_PATH="/root/ntsb/data/csvs/"
 
-python3 manage.py 2010_accident > /var/log/2010_accident.txt
-python3 manage.py 2010_vehicle > /var/log/2010_vehicle.txt
-python3 manage.py 2010_parkwork > /var/log/2010_parkwork.txt
-python3 manage.py 2010_person > /var/log/2010_person.txt
-python3 manage.py 2010_cevent > /var/log/2010_cevent.txt
-python3 manage.py 2010_crashrf > /var/log/2010_crashrf.txt
-python3 manage.py 2010_weather > /var/log/2010_weather.txt
-python3 manage.py 2010_damage > /var/log/2010_damage.txt
-python3 manage.py 2010_drimpair > /var/log/2010_drimpair.txt
-python3 manage.py 2010_factor > /var/log/2010_factor.txt
+python3 manage.py 2009_accident > /var/log/2009_accident.txt
+python3 manage.py 2009_vehicle > /var/log/2009_vehicle.txt
+python3 manage.py 2009_parkwork > /var/log/2009_parkwork.txt
+python3 manage.py 2009_person > /var/log/2009_person.txt
+python3 manage.py 2009_cevent > /var/log/2009_cevent.txt
+python3 manage.py 2009_crashrf > /var/log/2009_crashrf.txt
+python3 manage.py 2009_weather > /var/log/2009_weather.txt
+python3 manage.py 2009_damage > /var/log/2009_damage.txt
+python3 manage.py 2009_drimpair > /var/log/2009_drimpair.txt
+python3 manage.py 2009_factor > /var/log/2009_factor.txt
 python3 manage.py 2013_maneuver > /var/log/2013_maneuver.txt
 python3 manage.py 2012_maneuver > /var/log/2012_maneuver.txt
 python3 manage.py 2011_maneuver > /var/log/2011_maneuver.txt
 python3 manage.py 2010_maneuver > /var/log/2010_maneuver.txt
-python3 manage.py 2010_violatn > /var/log/2010_violatn.txt
-python3 manage.py 2010_vision > /var/log/2010_vision.txt
-python3 manage.py 2010_vehiclesf > /var/log/2010_vehiclesf.txt
-python3 manage.py 2010_driverrf > /var/log/2010_driverrf.txt
-python3 manage.py 2010_pvehiclesf > /var/log/2010_pvehiclesf.txt
-python3 manage.py 2010_drugs > /var/log/2010_drugs.txt
-python3 manage.py 2010_race > /var/log/2010_race.txt
-python3 manage.py 2010_personrf > /var/log/2010_personrf.txt
+python3 manage.py 2009_maneuver > /var/log/2009_maneuver.txt
+python3 manage.py 2009_violatn > /var/log/2009_violatn.txt
+python3 manage.py 2009_vision > /var/log/2009_vision.txt
+python3 manage.py 2009_vehiclesf > /var/log/2009_vehiclesf.txt
+python3 manage.py 2009_driverrf > /var/log/2009_driverrf.txt
+python3 manage.py 2009_pvehiclesf > /var/log/2009_pvehiclesf.txt
+python3 manage.py 2009_drugs > /var/log/2009_drugs.txt
+python3 manage.py 2009_race > /var/log/2009_race.txt
+python3 manage.py 2009_personrf > /var/log/2009_personrf.txt
