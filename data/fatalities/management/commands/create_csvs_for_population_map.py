1d2260f380f9e82430385d6adbbffa0177e2904d
bencarneiro <bencarneiro@gmail.com>
2025-02-01 09:58:30 -0600

    fix script to write csvs

--- data/fatalities/management/commands/create_csvs_for_population_map.py
+++ data/fatalities/management/commands/create_csvs_for_population_map.py
@@ -6,7 +6,10 @@ class Command(BaseCommand):
     def handle(self, *args, **kwasrgs):
         states = State.objects.all()
         for s in states:
+            print(s.name)
+            print(s.id)
             # Write CSV file
+            print("total")
             with open(f"../population/csvs/total/total_{s.id}.csv", "wt") as fp:
                 state_crashes = Accident.objects.filter(state=s)
                 writer = csv.writer(fp, delimiter=",")
@@ -14,23 +17,23 @@ class Command(BaseCommand):
                 # writer.writerow(["your", "header", "foo"])  # write header
                 for crash in state_crashes:
                     writer.writerow([crash.st_case, crash.fatalitytotals.total_fatalities, crash.month, crash.year, crash.day, crash.latitude, crash.longitude])
-
+            print("vehicle")
             with open(f"../population/csvs/vehicle/vehicle_{s.id}.csv", "wt") as fp:
                 state_crashes = Accident.objects.filter(state=s, fatalitytotals__vehicle_fatalities__gte=1)
                 writer = csv.writer(fp, delimiter=",")
                 writer.writerow(["st_case", "fatalities", "month", "year", "day", "LATITUDE", "LONGITUDE"])
                 # writer.writerow(["your", "header", "foo"])  # write header
                 for crash in state_crashes:
-                    writer.writerow([crash.st_case, crash.fatalitytotals.total_fatalities, crash.month, crash.year, crash.day, crash.latitude, crash.longitude])
-
+                    writer.writerow([crash.st_case, crash.fatalitytotals.vehicle_fatalities, crash.month, crash.year, crash.day, crash.latitude, crash.longitude])
+            print("nonmotorist")
             with open(f"../population/csvs/nonmotorist/nonmotorist_{s.id}.csv", "wt") as fp:
                 state_crashes = Accident.objects.filter(state=s, fatalitytotals__nonmotorist_fatalities__gte=1)
                 writer = csv.writer(fp, delimiter=",")
                 writer.writerow(["st_case", "fatalities", "month", "year", "day", "LATITUDE", "LONGITUDE"])
                 # writer.writerow(["your", "header", "foo"])  # write header
                 for crash in state_crashes:
-                    writer.writerow([crash.st_case, crash.fatalitytotals.total_fatalities, crash.month, crash.year, crash.day, crash.latitude, crash.longitude])
-            break
+                    writer.writerow([crash.st_case, crash.fatalitytotals.nonmotorist_fatalities, crash.month, crash.year, crash.day, crash.latitude, crash.longitude])
+            # break
         # wrecks = Accident.objects.all()
         # for w in wrecks:
 
