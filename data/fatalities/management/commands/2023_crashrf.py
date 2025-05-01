ccf0b07f8d82cf75292f74309963294a334315c0
bencarneiro <bencarneiro@gmail.com>
2025-05-01 16:07:10 -0500

    add 2023 import script for crash related factors table

---
+++ data/fatalities/management/commands/2023_crashrf.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from data.settings import CSV_PATH
+import pandas as pd
+from fatalities.models import CrashRelatedFactors, Accident
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwasrgs):
+        CrashRelatedFactors.objects.filter(accident__year=2023).delete()
+        csv = pd.read_csv(f"{CSV_PATH}2023/FARS2023NationalCSV/crashrf.csv", encoding='latin-1')
+        for x in csv.index:
+            
+            accident = Accident.objects.get(year=2023, st_case=csv['ST_CASE'][x])
+
+            st_case = str(csv['ST_CASE'][x])
+            if len(st_case) == 5:
+                st_case = "0" + st_case
+            number_of_saved_factors = len(CrashRelatedFactors.objects.filter(accident=accident))
+            new_factor_id = str(number_of_saved_factors + 1)
+            while len(new_factor_id) < 3:
+                new_factor_id = "0" + new_factor_id
+            primary_key = f"2023{st_case}{new_factor_id}"
+
+            data_to_save = {
+                "id": primary_key,
+                "accident": accident,
+                "crash_related_factor": csv['CRASHRF'][x]
+            }
+            print(data_to_save)
+            CrashRelatedFactors.objects.create(**data_to_save)
