fc13b12f5e411fea5d0fd1aacde2ae604767e584
bencarneiro <bencarneiro@gmail.com>
2025-08-21 13:52:01 -0500

    optimize 2000 drugs import script to use bulk import

--- data/fatalities/management/commands/2000_drugs.py
+++ data/fatalities/management/commands/2000_drugs.py
@@ -11,7 +11,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwasrgs):
         Drugs.objects.filter(person__accident__year=2000).delete()
         csv = pd.read_csv(f"{CSV_PATH}2000/PERSON.CSV", encoding='latin-1')
+        bulk_data_upload = []
         for x in csv.index:
+            if x % 1000 == 999:
+                print(new_drug_object)
+                Drugs.objects.bulk_create(bulk_data_upload)
+                bulk_data_upload = []
+                print("WE HIT THE DB")
             if csv['VEH_NO'][x] == 0:
                 person = Person.objects.get(accident__year=2000, person_number=csv['PER_NO'][x], accident__st_case=csv['ST_CASE'][x], vehicle__vehicle_number__isnull=True, parked_vehicle__vehicle_number__isnull=True)
             else:
@@ -26,17 +32,27 @@ class Command(BaseCommand):
             per_no = str(csv['PER_NO'][x])
             while len(per_no) < 3:
                 per_no = "0" + per_no
-
+            number_of_drugs_saved = 0
             for i in [1,2,3]:
                 drug_test_type_variable_name = f"DRUGTST{i}"
                 drug_test_result_variable_name = f"DRUGRES{i}"
-                number_saved = len(Drugs.objects.filter(person=person))
-                new_drugs_id = str(number_saved + 1)
+                # number_saved = len(Drugs.objects.filter(person=person))
+                new_drugs_id = str(number_of_drugs_saved + 1)
                 while len(new_drugs_id) < 3:
                     new_drugs_id = "0" + new_drugs_id
                 primary_key = f"2000{st_case}{veh_no}{per_no}{new_drugs_id}"
-                data_to_save = {"id": primary_key, "person": person}
-                data_to_save['drug_test_type'] = drug_test_type_converter(csv[drug_test_type_variable_name][x], 2000)
-                data_to_save['drug_test_results'] = csv[drug_test_result_variable_name][x]
-                print(data_to_save)
-                Drugs.objects.create(**data_to_save)
+                # data_to_save = {"id": primary_key, "person": person}
+                # data_to_save['drug_test_type'] = drug_test_type_converter(csv[drug_test_type_variable_name][x], 2000)
+                # data_to_save['drug_test_results'] = csv[drug_test_result_variable_name][x]
+                # print(data_to_save)
+                # Drugs.objects.create(**data_to_save)
+                new_drug_object = Drugs(id=primary_key, 
+                                        person=person, 
+                                        drug_test_type=drug_test_type_converter(csv[drug_test_type_variable_name][x], 2000), 
+                                        drug_test_results=csv[drug_test_result_variable_name][x]
+                                        )
+                bulk_data_upload += [new_drug_object]
+                number_of_drugs_saved += 1
+
+        Drugs.objects.bulk_create(bulk_data_upload)
+        print(f"we hit it again")
