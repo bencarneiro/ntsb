146b767833c2bbf63add71525847311d2a5e779b
bencarneiro <bencarneiro@gmail.com>
2025-08-27 11:41:04 -0500

    add a bash script which is gonna execute 1995-1999 on the server

---
+++ data/import_1995_1999.sh
@@ -0,0 +1,35 @@
+export CSV_PATH="/root/ntsb/data/csvs/"
+
+python3 manage.py 1999_accident > /var/log/1999_accident.txt
+python3 manage.py 1999_vehicle > /var/log/1999_vehicle.txt
+python3 manage.py 1999_person > /var/log/1999_person.txt
+python3 manage.py 1999_crashrf > /var/log/1999_crashrf.txt
+python3 manage.py 1999_weather > /var/log/1999_weather.txt
+python3 manage.py 1999_damage > /var/log/1999_damage.txt
+python3 manage.py 1999_drimpair > /var/log/1999_drimpair.txt
+python3 manage.py 1999_factor > /var/log/1999_factor.txt
+python3 manage.py 1999_maneuver > /var/log/1999_maneuver.txt
+python3 manage.py 1999_violatn > /var/log/1999_violatn.txt
+python3 manage.py 1999_vision > /var/log/1999_vision.txt
+python3 manage.py 1999_vehiclesf > /var/log/1999_vehiclesf.txt
+python3 manage.py 1999_driverrf > /var/log/1999_driverrf.txt
+python3 manage.py 1999_drugs > /var/log/1999_drugs.txt
+python3 manage.py 1999_race > /var/log/1999_race.txt
+python3 manage.py 1999_personrf > /var/log/1999_personrf.txt
+
+
+python3 manage.py 1998_accident > /var/log/1998_accident.txt
+python3 manage.py 1998_vehicle > /var/log/1998_vehicle.txt
+python3 manage.py 1998_person > /var/log/1998_person.txt
+python3 manage.py 1998_crashrf > /var/log/1998_crashrf.txt
+python3 manage.py 1998_weather > /var/log/1998_weather.txt
+python3 manage.py 1998_damage > /var/log/1998_damage.txt
+python3 manage.py 1998_drimpair > /var/log/1998_drimpair.txt
+python3 manage.py 1998_factor > /var/log/1998_factor.txt
+python3 manage.py 1998_maneuver > /var/log/1998_maneuver.txt
+python3 manage.py 1998_violatn > /var/log/1998_violatn.txt
+python3 manage.py 1998_vision > /var/log/1998_vision.txt
+python3 manage.py 1998_vehiclesf > /var/log/1998_vehiclesf.txt
+python3 manage.py 1998_driverrf > /var/log/1998_driverrf.txt
+python3 manage.py 1998_drugs > /var/log/1998_drugs.txt
+python3 manage.py 1998_personrf > /var/log/1998_personrf.txt
