c731f8f99ba0f7ca3cb8f0584c16397fee3208f4
bencarneiro <bencarneiro@gmail.com>
2025-08-27 16:27:38 -0500

    update bash script to import five whole years of data

--- data/import_1995_1999.sh
+++ data/import_1995_1999.sh
@@ -33,3 +33,54 @@ python3 manage.py 1998_vehiclesf > /var/log/1998_vehiclesf.txt
 python3 manage.py 1998_driverrf > /var/log/1998_driverrf.txt
 python3 manage.py 1998_drugs > /var/log/1998_drugs.txt
 python3 manage.py 1998_personrf > /var/log/1998_personrf.txt
+
+
+python3 manage.py 1997_accident > /var/log/1997_accident.txt
+python3 manage.py 1997_vehicle > /var/log/1997_vehicle.txt
+python3 manage.py 1997_person > /var/log/1997_person.txt
+python3 manage.py 1997_crashrf > /var/log/1997_crashrf.txt
+python3 manage.py 1997_weather > /var/log/1997_weather.txt
+python3 manage.py 1997_damage > /var/log/1997_damage.txt
+python3 manage.py 1997_drimpair > /var/log/1997_drimpair.txt
+python3 manage.py 1997_factor > /var/log/1997_factor.txt
+python3 manage.py 1997_maneuver > /var/log/1997_maneuver.txt
+python3 manage.py 1997_violatn > /var/log/1997_violatn.txt
+python3 manage.py 1997_vision > /var/log/1997_vision.txt
+python3 manage.py 1997_vehiclesf > /var/log/1997_vehiclesf.txt
+python3 manage.py 1997_driverrf > /var/log/1997_driverrf.txt
+python3 manage.py 1997_drugs > /var/log/1997_drugs.txt
+python3 manage.py 1997_personrf > /var/log/1997_personrf.txt
+
+
+python3 manage.py 1996_accident > /var/log/1996_accident.txt
+python3 manage.py 1996_vehicle > /var/log/1996_vehicle.txt
+python3 manage.py 1996_person > /var/log/1996_person.txt
+python3 manage.py 1996_crashrf > /var/log/1996_crashrf.txt
+python3 manage.py 1996_weather > /var/log/1996_weather.txt
+python3 manage.py 1996_damage > /var/log/1996_damage.txt
+python3 manage.py 1996_drimpair > /var/log/1996_drimpair.txt
+python3 manage.py 1996_factor > /var/log/1996_factor.txt
+python3 manage.py 1996_maneuver > /var/log/1996_maneuver.txt
+python3 manage.py 1996_violatn > /var/log/1996_violatn.txt
+python3 manage.py 1996_vision > /var/log/1996_vision.txt
+python3 manage.py 1996_vehiclesf > /var/log/1996_vehiclesf.txt
+python3 manage.py 1996_driverrf > /var/log/1996_driverrf.txt
+python3 manage.py 1996_drugs > /var/log/1996_drugs.txt
+python3 manage.py 1996_personrf > /var/log/1996_personrf.txt
+
+
+python3 manage.py 1995_accident > /var/log/1995_accident.txt
+python3 manage.py 1995_vehicle > /var/log/1995_vehicle.txt
+python3 manage.py 1995_person > /var/log/1995_person.txt
+python3 manage.py 1995_crashrf > /var/log/1995_crashrf.txt
+python3 manage.py 1995_weather > /var/log/1995_weather.txt
+python3 manage.py 1995_damage > /var/log/1995_damage.txt
+python3 manage.py 1995_drimpair > /var/log/1995_drimpair.txt
+python3 manage.py 1995_factor > /var/log/1995_factor.txt
+python3 manage.py 1995_maneuver > /var/log/1995_maneuver.txt
+python3 manage.py 1995_violatn > /var/log/1995_violatn.txt
+python3 manage.py 1995_vision > /var/log/1995_vision.txt
+python3 manage.py 1995_vehiclesf > /var/log/1995_vehiclesf.txt
+python3 manage.py 1995_driverrf > /var/log/1995_driverrf.txt
+python3 manage.py 1995_drugs > /var/log/1995_drugs.txt
+python3 manage.py 1995_personrf > /var/log/1995_personrf.txt
