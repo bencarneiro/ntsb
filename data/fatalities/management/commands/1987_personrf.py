340c4fd66fad9816e3558de25c708625009b819d
bencarneiro <bencarneiro@gmail.com>
2025-09-07 08:14:33 -0500

    fix person related factor import scripts for about a decade

--- data/fatalities/management/commands/1987_personrf.py
+++ data/fatalities/management/commands/1987_personrf.py
@@ -9,8 +9,8 @@ from django.db.models import Q
 
 class Command(BaseCommand):
     def handle(self, *args, **kwasrgs):
-        PersonRelatedFactor.objects.filter(person__accident__year=1996).delete()
-        csv = pd.read_csv(f"{CSV_PATH}1996/PERSON.CSV", encoding='latin-1')
+        PersonRelatedFactor.objects.filter(person__accident__year=1987).delete()
+        csv = pd.read_csv(f"{CSV_PATH}1987/PERSON.CSV", encoding='latin-1')
         bulk_data_upload=[]
         for x in csv.index:
             if x % 1000 == 999:
@@ -19,9 +19,9 @@ class Command(BaseCommand):
                 bulk_data_upload = []
                 print("WE HIT THE DB")
             if csv['VEH_NO'][x] == 0:
-                person = Person.objects.get(accident__year=1996, person_number=csv['PER_NO'][x], accident__st_case=csv['ST_CASE'][x], vehicle__vehicle_number__isnull=True, parked_vehicle__vehicle_number__isnull=True)
+                person = Person.objects.get(accident__year=1987, person_number=csv['PER_NO'][x], accident__st_case=csv['ST_CASE'][x], vehicle__vehicle_number__isnull=True, parked_vehicle__vehicle_number__isnull=True)
             else:
-                person = Person.objects.get(Q(accident__year=1996), Q(person_number=csv['PER_NO'][x]),  Q(accident__st_case=csv['ST_CASE'][x]), Q(vehicle__vehicle_number=csv['VEH_NO'][x])|Q(parked_vehicle__vehicle_number=csv['VEH_NO'][x]))
+                person = Person.objects.get(Q(accident__year=1987), Q(person_number=csv['PER_NO'][x]),  Q(accident__st_case=csv['ST_CASE'][x]), Q(vehicle__vehicle_number=csv['VEH_NO'][x])|Q(parked_vehicle__vehicle_number=csv['VEH_NO'][x]))
             
             st_case = str(csv['ST_CASE'][x])
             if len(st_case) == 5:
@@ -38,17 +38,17 @@ class Command(BaseCommand):
                 new_factor_id = str(number_of_factors_saved + 1)
                 while len(new_factor_id) < 3:
                     new_factor_id = "0" + new_factor_id
-                primary_key = f"1996{st_case}{veh_no}{per_no}{new_factor_id}"
+                primary_key = f"1987{st_case}{veh_no}{per_no}{new_factor_id}"
                 
                 # data_to_save = {"id": primary_key, "person": person}
-                # factor_code = person_related_factor_converter(csv[factor][x], 1996)
+                # factor_code = person_related_factor_converter(csv[factor][x], 1987)
                 # if factor_code:
                 #     data_to_save['person_related_factor'] = factor_code
                 #     print(data_to_save)
                 #     PersonRelatedFactor.objects.create(**data_to_save)
 
 
-                factor_code = person_related_factor_converter(csv[factor][x], 1996)
+                factor_code = person_related_factor_converter(csv[factor][x], 1987)
                 if factor_code:
                     number_of_factors_saved += 1
                     new_personrf_object = PersonRelatedFactor(id=primary_key, person=person, person_related_factor=factor_code)
