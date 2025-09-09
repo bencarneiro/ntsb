57a655fb5851d63a6f04ef67bb6630a05301186c
bencarneiro <bencarneiro@gmail.com>
2025-09-08 19:18:12 -0500

    move last batch of import scripts into archive

--- data/fatalities/management/commands/fatality_totals.py
+++ data/fatalities/management/commands/fatality_totals.py
@@ -4,7 +4,7 @@ from fatalities.models import Accident, FatalityTotals
 class Command(BaseCommand):
     def handle(self, *args, **kwasrgs):
         # FatalityTotals.objects.all().delete()
-        for year in range(1985,1990):
+        for year in range(1991,1995):
             for a in Accident.objects.filter(year=year):
                 people = a.person_set.filter(injury_severity=4)
 
