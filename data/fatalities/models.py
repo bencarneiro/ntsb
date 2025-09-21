ef762e4f7c4e48e3c5c16860605d30e13a679f0f
bencarneiro <bencarneiro@gmail.com>
2025-09-20 19:13:24 -0500

    finish design changes to crash report page

--- data/fatalities/models.py
+++ data/fatalities/models.py
@@ -48,7 +48,7 @@ class Accident(models.Model):
     def link(self):
         return f"<a href='/accidents/{self.id}'>Details Here</a>"
     def map_link(self):
-        return f"<a href='https://www.google.com/maps/search/?api=1&query={self.latitude},{self.longitude}'>Google Maps</a>"
+        return f"<a href='https://www.google.com/maps/search/?api=1&query={self.latitude},{self.longitude}'>({self.latitude}, {self.longitude})</a>"
     
     id = models.PositiveBigIntegerField(primary_key = True)
     year = models.PositiveSmallIntegerField(null=False, blank=False)
