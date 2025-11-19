1be539bd248c6f8e2823e8d519a117193fc6ee66
bencarneiro <bencarneiro@gmail.com>
2025-11-18 20:24:26 -0600

    add bash script to import all the penn data

---
+++ data/import_pennsylvania.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+export PENNSYLVANIA_PATH="/root/ntsb/pennsylvania/"
+python3 manage.py pennsylvania_2024 > /var/log/pennsylvania_2024.txt
+python3 manage.py pennsylvania_2023 > /var/log/pennsylvania_2023.txt
+python3 manage.py pennsylvania_2022 > /var/log/pennsylvania_2022.txt
+python3 manage.py pennsylvania_2021 > /var/log/pennsylvania_2021.txt
+python3 manage.py pennsylvania_2020 > /var/log/pennsylvania_2020.txt
+python3 manage.py pennsylvania_2019 > /var/log/pennsylvania_2019.txt
+python3 manage.py pennsylvania_2018 > /var/log/pennsylvania_2018.txt
+python3 manage.py pennsylvania_2017 > /var/log/pennsylvania_2017.txt
+python3 manage.py pennsylvania_2016 > /var/log/pennsylvania_2016.txt
+python3 manage.py pennsylvania_2015 > /var/log/pennsylvania_2015.txt
+python3 manage.py pennsylvania_2014 > /var/log/pennsylvania_2014.txt
+python3 manage.py pennsylvania_2013 > /var/log/pennsylvania_2013.txt
+python3 manage.py pennsylvania_2012 > /var/log/pennsylvania_2012.txt
+python3 manage.py pennsylvania_2011 > /var/log/pennsylvania_2011.txt
+python3 manage.py pennsylvania_2010 > /var/log/pennsylvania_2010.txt
+python3 manage.py pennsylvania_2009 > /var/log/pennsylvania_2009.txt
+python3 manage.py pennsylvania_2008 > /var/log/pennsylvania_2008.txt
+python3 manage.py pennsylvania_2007 > /var/log/pennsylvania_2007.txt
+python3 manage.py pennsylvania_2006 > /var/log/pennsylvania_2006.txt
+python3 manage.py pennsylvania_2005 > /var/log/pennsylvania_2005.txt
