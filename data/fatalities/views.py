0f6e8fdb1ea58a2a771c8b18a146a7316b634b9c
Ben Carneiro <bencarneiro@gmail.com>
2026-09-08 14:31:24 -0500

    create a massachusetts map, csv download views for mass data, script to generate maptiles

--- data/fatalities/views.py
+++ data/fatalities/views.py
@@ -1111,6 +1111,67 @@ def arizona_fatality_csv_2(request):
     return redirect("/")
 
 
+
+def massachusetts_fatality_csv(request):
+    if request.user.is_authenticated:  
+        # Create the HttpResponse object with the appropriate CSV header.
+        # year = request.GET['year']
+        response = HttpResponse(
+            content_type="text/csv",
+            headers={"Content-Disposition": f'attachment; filename="massachusetts_fatalities.csv"'},
+        )
+
+        writer = csv.writer(response)
+        writer.writerow(["st_case", "fatalities", "serious_injuries", "dt", "LATITUDE", "LONGITUDE"])
+
+        crashes = Accident.objects.filter(state_id=25)
+        for crash in crashes:
+            injury_count = len(Person.objects.filter(accident=crash, injury_severity=3))
+            writer.writerow([crash.st_case, crash.fatalitytotals.total_fatalities, injury_count, crash.datetime, crash.latitude, crash.longitude])
+
+        return response
+    return redirect("/")
+
+
+
+def massachusetts_injury_csv(request):
+    if request.user.is_authenticated:  
+    # Create the HttpResponse object with the appropriate CSV header.
+    
+        response = HttpResponse(
+            content_type="text/csv",
+            headers={"Content-Disposition": f'attachment; filename="massachusetts_injuries.csv"'},
+        )
+
+        writer = csv.writer(response)
+        writer.writerow(["id", "fatalities", "serious_injuries", "dt", "LATITUDE", "LONGITUDE"])
+
+        crashes = InjuryAccident.objects.filter(death_count=0, state_id=25)
+        for crash in crashes:
+            writer.writerow([crash.id, crash.death_count, crash.severe_injury_count, crash.dt, crash.latitude, crash.longitude])
+
+        return response
+    return redirect("/")
+
+
+def massachusetts_fatality_csv_2(request):
+    if request.user.is_authenticated:  
+    # Create the HttpResponse object with the appropriate CSV header.
+        response = HttpResponse(
+            content_type="text/csv",
+            headers={"Content-Disposition": f'attachment; filename="massachusetts_fatalities_2.csv"'},
+        )
+        writer = csv.writer(response)
+        writer.writerow(["id", "fatalities", "serious_injuries", "dt", "LATITUDE", "LONGITUDE"])
+        crashes = InjuryAccident.objects.filter(death_count__gte=1, state_id=25, dt__year__gte=2025)
+        for crash in crashes:
+            writer.writerow([crash.id, crash.death_count, crash.severe_injury_count, crash.dt, crash.latitude, crash.longitude])
+
+        return response
+    return redirect("/")
+
+
+
 def tennessee_fatality_csv(request):
     if request.user.is_authenticated:  
         # Create the HttpResponse object with the appropriate CSV header.
@@ -1635,6 +1696,9 @@ def florida(request):
 
 def arizona(request):
     return render(request, "arizona.html", {"TILES_URL": TILES_URL})
+
+def massachusetts(request):
+    return render(request, "massachusetts.html", {"TILES_URL": TILES_URL})
 
 def tennessee(request):
     return render(request, "tennessee.html", {"TILES_URL": TILES_URL})
